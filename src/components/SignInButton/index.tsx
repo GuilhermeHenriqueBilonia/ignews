16a50575d9244122515a72b0596900340ff363ce
Guilherme Bilonia <guilherme.henrique@devexsolucoes.com.br>
2021-08-10 09:06:51 -0300

    ignews completo

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,28 @@
+import {FaGithub} from 'react-icons/fa'
+import {FiX} from 'react-icons/fi'
+import {signIn, signOut, useSession} from 'next-auth/client'
+
+import styles from './styles.module.scss'
+
+
+
+export default function SignInButton() {
+    const [session] = useSession();
+
+    console.log(session);
+
+    return session ?
+    (
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" onClick={() => signOut()} className={styles.closeIcon}/>
+        </button>
+    ) :
+    (
+        <button type="button" onClick={() => signIn('github')} className={styles.signInButton}>
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
+    )
+}
